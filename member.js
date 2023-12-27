b7dc52b4daa11fc6de502d8eb7e314b3eb5f771e
Sunny✨ <Sundaypius2000@gmail.com>
2023-12-27 00:45:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    
+    var member = new Object();
+    
+    member.name = "John Doe";
+    member.age = 25;
+    member.skills = ["HTML", "CSS", "JavaScript"];
+    
+    return member;
+}
